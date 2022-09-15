7ef3180d2894e309687952187019d378b930b531
ksv <krjukov_s@mail.ru>
2022-09-15 10:42:38 +0400

    revision of the waveButton

--- src/components/waveButton/WaveButton.js
+++ src/components/waveButton/WaveButton.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import style from './waveButton.module.css'
+import React from "react";
+import style from "./waveButton.module.css";
 
 export const WaveButton = () => {
   return (
     <>
       <div className={style.container}>
-        <a href='/' className={style.wvBtn}>
-        <span className={style.spa}>waveBtn</span>
-        <div className={style.liquid}></div>
+        <a href="/" className={style.wvBtn}>
+          <span className={style.spa}>жми</span>
+          <div className={style.liquid}></div>
         </a>
       </div>
     </>
-  )
-}
+  );
+};
