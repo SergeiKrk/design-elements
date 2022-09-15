348182fb6cf80c4df0cf038dd8d6b237a0d95978
ksv <krjukov_s@mail.ru>
2022-09-15 17:52:49 +0400

    revision of the navBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,27 +1,29 @@
 import React from "react";
-import style from "./navBar.module.css";
+import s from "./navBar.module.css";
 
 export const NavBar = () => {
   return (
-    <div className={style.navBar}>
-      <a href="/">Logo</a>
-      <div className={style.menuToggle}>
-        <nav>
-          <ul>
-            <li>
-              <a href="/" className={style.active}>
-                Главная
-              </a>
-            </li>
-            <li>
-              <a href="/?#">О Нас</a>
-            </li>
-            <li>
-              <a href="/?##">Контакты</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className={s.navBar}>
+      <a className={s.logo} href="/">
+        Logo
+      </a>
+      <div className={s.menuToggle}>A</div>
+      <nav className={s.nav}>
+        <ul>
+          <li>
+            <a href="/" className={s.active}>
+              Главная
+            </a>
+          </li>
+          <li>
+            <a href="/?#">О Нас</a>
+          </li>
+          <li>
+            <a href="/?##">Контакты</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={s.clearfix}></div>
     </div>
   );
 };
